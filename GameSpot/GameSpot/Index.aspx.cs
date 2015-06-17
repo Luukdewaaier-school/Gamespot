b8d413ce1421c0f132f705c744fd841985b7864a
Luukdewaaier <luukoland@hotmail.com>
2015-06-17 22:35:17 +0200

    start up

---
+++ GameSpot/GameSpot/Index.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GameSpot
+{
+    public partial class Index : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //tb.Click += new EventHandler(dasdas, hoi);
+        }
+
+        protected void hoi()
+        {
+            
+        }
+    }
+}
